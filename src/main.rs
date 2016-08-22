bf1b47803286bb844a1fcf960d08a4887212db7b
Josh Weberruss <josh.weberruss@monash.edu>
2016-08-22 15:51:53 +1000

    Basic BST insertion working

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ pub struct TreeNode<K,V> where K: Ord+Copy, V: Copy {
 }
 
 impl<K,V> Tree<K,V> where K: Ord+Copy, V: Copy {
+	pub fn new() {
+		Tree {root: Rc::new(None)}
+	}
+
 	pub fn find(&self, search_key: K) -> Option<&V> {
 		let mut current = self.root.deref();
 		
@@ -34,8 +38,8 @@ impl<K,V> Tree<K,V> where K: Ord+Copy, V: Copy {
 				}
 			};
 			/*match current {
-				&Tree::Leaf => return None,
-				&Tree::Node(ref node) => {
+				&None => return None,
+				&Some(ref node) => {
 					match search_key.cmp(&node.key) {
 						Ordering::Less => current = node.left.deref(),
 						Ordering::Greater => current = node.right.deref(),
@@ -48,27 +52,27 @@ impl<K,V> Tree<K,V> where K: Ord+Copy, V: Copy {
 
 	/*fn is_red(&self) -> bool {
 		match self {
-			&Tree::Leaf => false,
-			&Tree::Node(ref node) => {
+			&None => false,
+			&Some(ref node) => {
 				node.is_red
 			}
 		}
 	}*/
 
 	pub fn insert(&self, key: K, val: V) -> Option<Tree<K,V>> {
-		panic!();
+		Self::rec_insert(key, val, &self.root, None).map(|r| Tree {root: r})
 	}
 
 	fn rec_insert(key: K, val: V, current: &Rc<Option<TreeNode<K,V>>>, parent: Option<&Rc<Option<TreeNode<K,V>>>>) -> Option<Rc<Option<TreeNode<K,V>>>>{
 		match current.deref() {
 			&None => {
 				//insert here
-				Some(Rc::new(Some(Tree::Node(TreeNode{
+				Some(Rc::new(Some(TreeNode{
 					is_red: true,
 					key: key,
 					val: val,
-					left: Rc::new(Tree::Leaf),
-					right: Rc::new(Tree::Leaf)
+					left: Rc::new(None),
+					right: Rc::new(None)
 				})))
 			},
 			&Some(ref node) => {
@@ -76,7 +80,7 @@ impl<K,V> Tree<K,V> where K: Ord+Copy, V: Copy {
 					Ordering::Less => {
 						let new_left_child = Self::rec_insert(key, val, &node.left, Some(current));
 						new_left_child.map(|c| {
-							Rc::new(Tree::Node(TreeNode{
+							Rc::new(Some(TreeNode{
 								is_red: node.is_red,
 								key: node.key,
 								val: node.val,
@@ -88,7 +92,7 @@ impl<K,V> Tree<K,V> where K: Ord+Copy, V: Copy {
 					Ordering::Greater => {
 						let new_right_child = Self::rec_insert(key, val, &node.right, Some(current));
 						new_right_child.map(|c| {
-							Rc::new(Tree::Node(TreeNode{
+							Rc::new(Some(TreeNode{
 								is_red: node.is_red,
 								key: node.key,
 								val: node.val,
@@ -105,81 +109,81 @@ impl<K,V> Tree<K,V> where K: Ord+Copy, V: Copy {
 }
 
 fn main() {
-    let tree = Tree::Node(TreeNode {
+    let tree = Some(TreeNode {
 		is_red: false,
 		key: 13,
 		val: (),
-		left: Rc::new(Tree::Node(TreeNode {
+		left: Rc::new(Some(TreeNode {
 			is_red: true,
 			key: 8,
 			val: (),
-			left: Rc::new(Tree::Node(TreeNode {
+			left: Rc::new(Some(TreeNode {
 				is_red: false,
 				key: 1,
 				val: (),
-				left: Rc::new(Tree::Leaf),
-				right: Rc::new(Tree::Node(TreeNode{
+				left: Rc::new(None),
+				right: Rc::new(Some(TreeNode{
 					is_red: true,
 					key: 6,
 					val: (),
-					left: Rc::new(Tree::Leaf),
-					right: Rc::new(Tree::Leaf)
+					left: Rc::new(None),
+					right: Rc::new(None)
 				}))
 			})),
-			right: Rc::new(Tree::Node(TreeNode {
+			right: Rc::new(Some(TreeNode {
 				is_red: false,
 				key: 11,
 				val: (),
-				left: Rc::new(Tree::Leaf),
-				right: Rc::new(Tree::Leaf)
+				left: Rc::new(None),
+				right: Rc::new(None)
 			}))
 		})),
-		right: Rc::new(Tree::Node(TreeNode {
+		right: Rc::new(Some(TreeNode {
 			is_red: true,
 			key: 17,
 			val: (),
-			left: Rc::new(Tree::Node(TreeNode {
+			left: Rc::new(Some(TreeNode {
 				is_red: false,
 				key: 15,
 				val: (),
-				left: Rc::new(Tree::Leaf),
-				right: Rc::new(Tree::Leaf)
+				left: Rc::new(None),
+				right: Rc::new(None)
 			})),
-			right: Rc::new(Tree::Node(TreeNode {
+			right: Rc::new(Some(TreeNode {
 				is_red: false,
 				key: 25,
 				val: (),
-				left: Rc::new(Tree::Node(TreeNode {
+				left: Rc::new(Some(TreeNode {
 					is_red: true,
 					key: 22,
 					val: (),
-					left: Rc::new(Tree::Leaf),
-					right: Rc::new(Tree::Leaf)
+					left: Rc::new(None),
+					right: Rc::new(None)
 				})),
-				right: Rc::new(Tree::Node(TreeNode {
+				right: Rc::new(Some(TreeNode {
 					is_red: true,
 					key: 27,
 					val: (),
-					left: Rc::new(Tree::Leaf),
-					right: Rc::new(Tree::Leaf)
+					left: Rc::new(None),
+					right: Rc::new(None)
 				}))
 			}))
 		}))
 	});
 
-	let tree2 = Tree::Node(TreeNode {
+	let tree2 = Tree { root: Rc::new(Some(TreeNode {
 		is_red: false,
 		key: 1,
 		val: (),
-		left: Rc::new(Tree::Leaf),
-		right: Rc::new(Tree::Node(TreeNode{
+		left: Rc::new(None),
+		right: Rc::new(Some(TreeNode{
 			is_red: true,
 			key: 6,
 			val: (),
-			left: Rc::new(Tree::Leaf),
-			right: Rc::new(Tree::Leaf)
+			left: Rc::new(None),
+			right: Rc::new(None)
 		}))
-	});
+	}))};
 
 	let tree3 = tree2.insert(0, ());
 	println!("{:#?}", tree3);
@@ -192,19 +196,19 @@ mod tests {
 
 	#[test]
 	fn test_find(){
-		let tree = Tree::Node(TreeNode {
+		let tree = Tree{root: Rc::new(Some(TreeNode {
 			is_red: false,
 			key: 1,
 			val: (),
-			left: Rc::new(Tree::Leaf),
-			right: Rc::new(Tree::Node(TreeNode{
+			left: Rc::new(None),
+			right: Rc::new(Some(TreeNode{
 				is_red: true,
 				key: 6,
 				val: (),
-				left: Rc::new(Tree::Leaf),
-				right: Rc::new(Tree::Leaf)
+				left: Rc::new(None),
+				right: Rc::new(None)
 			}))
-		});
+		}))};
 
 		assert_eq!(tree.find(6), Some(&()));
 		assert_eq!(tree.find(1), Some(&()));
